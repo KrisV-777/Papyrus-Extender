1e0b6d2863ed123037e301e4e282173bfeddc68e
Scrabx3 <kris.vlntn@gmail.com>
2024-11-11 15:10:05 +0100

    refactor: move serialization functions to Serialization namespace and update ActorManager references

--- src/Serialize.cpp
+++ src/Serialize.cpp
@@ -1,28 +1,34 @@
 #include "Serialize.h"
 
-#include "Handlers/ActorManager.h"
+#include "PyramidUtils/ActorManager.h"
 
-void Serialize::Save(SKSE::SerializationInterface* a_intfc) {
-    if (!a_intfc->OpenRecord(RecordID, 0)) {
-        SKSE::log::error("Unable to open record to write cosave data.");
-        return;
-    }
+namespace Serialization
+{
+	void Serialize::Save(SKSE::SerializationInterface* a_intfc)
+	{
+		if (!a_intfc->OpenRecord(RecordID, 0)) {
+			SKSE::log::error("Unable to open record to write cosave data.");
+			return;
+		}
 
-    ActorManager::Serialize(a_intfc);
-}
+		PyramidUtils::ActorManager::Serialize(a_intfc);
+	}
 
-void Serialize::Load(SKSE::SerializationInterface* a_intfc) {
-    uint32_t type;
-    uint32_t version;
-    uint32_t length;
-    while (a_intfc->GetNextRecordInfo(type, version, length)) {
-        if (type == RecordID) {
-            ActorManager::Deserialize(a_intfc);
-            break;
-        }
-    }
-}
+	void Serialize::Load(SKSE::SerializationInterface* a_intfc)
+	{
+		uint32_t type;
+		uint32_t version;
+		uint32_t length;
+		while (a_intfc->GetNextRecordInfo(type, version, length)) {
+			if (type == RecordID) {
+				PyramidUtils::ActorManager::Deserialize(a_intfc);
+				break;
+			}
+		}
+	}
 
-void Serialize::Revert(SKSE::SerializationInterface* a_intfc) {
-    ActorManager::Revert(a_intfc);
-}
+	void Serialize::Revert(SKSE::SerializationInterface* a_intfc)
+	{
+		PyramidUtils::ActorManager::Revert(a_intfc);
+	}
+} // namespace Serialization
--- src/Serialize.h
+++ src/Serialize.h
@@ -1,11 +1,14 @@
 #pragma once
 
-class Serialize
+namespace Serialization
 {
-public:
-	static inline const auto RecordID = _byteswap_ulong('scpe');
+	class Serialize
+	{
+	public:
+		static inline const auto RecordID = _byteswap_ulong('scpe');
 
-	static void Save(SKSE::SerializationInterface* a_intfc);
-	static void Load(SKSE::SerializationInterface* a_intfc);
-	static void Revert(SKSE::SerializationInterface* a_intfc);
-};
+		static void Save(SKSE::SerializationInterface* a_intfc);
+		static void Load(SKSE::SerializationInterface* a_intfc);
+		static void Revert(SKSE::SerializationInterface* a_intfc);
+	};
+}	 // namespace Serialization
