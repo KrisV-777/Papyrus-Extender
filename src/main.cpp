9edf07ded570630717edd7a1ae60329377e737ea
Scrabx3 <kris.vlntn@gmail.com>
2024-11-11 08:39:33 +0100

    fix: adjust hook installation for non-VR runtime

--- src/main.cpp
+++ src/main.cpp
@@ -70,9 +70,8 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_s
 	// 	return false;
 	// }
 
-	#ifndef SKYRIMVR
+	if (REL::Module::GetRuntime() != REL::Module::Runtime::VR)
 		Hooks::Install();
-		#endif
 
 	const auto serialization = SKSE::GetSerializationInterface();
 	serialization->SetUniqueID(Serialize::RecordID);
